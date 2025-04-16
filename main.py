6d00f29483163480a04164db5edb3ba5674cff5b
Martin Crampon <martin.crampon@hotmail.fr>
2025-04-16 08:33:48 +0000

    feat(cli): implement CLI commands for greeting functionality and add empty __init__.py files

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import click
 
 
-# * Main program
+@click.group()
+def dpt():
+    """DPT - CLI is a command line interface for the Docker Project Templates.
+    """
+    pass
+
+
+@dpt.command("greet")
+@click.option("--count", default=1, help="number of greetings")
+@click.argument("name")
+def hello(count, name):
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+
 if __name__ == "__main__":
-
-    print("Hello World")
-    print("This is a test")
+    dpt()
